04469b24d4bc5c5cd280ed6dc97cb5956efffd05
dameeta <85731431+dameeta@users.noreply.github.com>
2022-02-28 15:48:38 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,21 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import ShowName from './ShowName';
-
+let btnText="Change name";
 function App() {
-  const[name,setName]=useState("Vikash");
+  const[name,setName]=useState("Sharan");
+  const[showMyName, setShowMyName]=useState(false);
+  const[students, setStudents]=useState([
+    {name: "Sharan", about: "Teaching React", abc: "sharan"},
+    {name: "John", about: "Learning React", abc: "john"},
+    {name: "Mona", about: "Learning React", abc: "mona"},
+    {name: "Preet", about: "Learning React", abc: "preet"}
 
+  ]);
   const changeMyName=()=>{
-    console.log("Button clicked");
-    setName("Some other name");
+   
+   setShowMyName(!showMyName);
   };
+
   return <div>
-      <h1>{name}</h1>
-      <button onClick={changeMyName}>Change name</button>
-      <ShowName name={name} about={"Learning react"}/>
-      <ShowName name={"Harpreet"}/>
+      <button onClick={changeMyName}>{btnText}</button>
+      {
+         students.map(students=>{
+           return <p><ShowName {...students} /></p>;
+         })
+      }
+      {/* showMyName && <ShowName name={name} about={"Learning react"}/>
+      <ShowName name={"Mona"}/>
       <ShowName name={"John"} about={"Learning js"} />
-      <ShowName name={"Preet"} about={"Learning Angular"} />
+      <ShowName name={"Preet"} about={"Learning Angular"} /> */}
 
     </div>;
   
