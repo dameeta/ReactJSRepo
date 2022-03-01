9b79efb3fb48a7478b4076f96bb64cf47db762c3
dameeta <85731431+dameeta@users.noreply.github.com>
2022-03-01 15:47:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Route,Link,BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  AboutUs  from './AboutUs';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = (
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Router>
+    <div>
+    <h1>This is React Router Demo!!</h1>
+<App />  
+    </div>
+  </Router>
+)
+ReactDOM.render(routing,document.getElementById('root'));
