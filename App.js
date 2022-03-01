af36a4794f1404afb71c7891fa8b4579dd0d20c2
dameeta <85731431+dameeta@users.noreply.github.com>
2022-03-01 15:46:18 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import ShowName from './ShowName';
+import React from 'react';
+import AboutUs from './AboutUs';
 
-function App() {
-  const[name,setName]=useState("Vikash");
+function App (){
 
-  const changeMyName=()=>{
-    console.log("Button clicked");
-    setName("Some other name");
-  };
-  return <div>
-      <h1>{name}</h1>
-      <button onClick={changeMyName}>Change name</button>
-      <ShowName name={name} about={"Learning react"}/>
-      <ShowName name={"Harpreet"}/>
-      <ShowName name={"John"} about={"Learning js"} />
-      <ShowName name={"Preet"} about={"Learning Angular"} />
+    return(
+      <div>
+        <h1>Home Page</h1>
+        <AboutUs />
+      </div>
 
-    </div>;
+    )
   
 }
-
-export default App;
+export default App
