2b4ce744a069e2716fc6d5cb4dc31d2c8314dae0
dameeta <85731431+dameeta@users.noreply.github.com>
2022-03-04 15:10:56 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -4,7 +4,7 @@ import React from "react";
 function Home() {
   return (
     <div className="container">
-    <div className="card" style="width: 18rem;">
+    <div className="card" style={{width: '18rem'}}>
   <img src="..." className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
