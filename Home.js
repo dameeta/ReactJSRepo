20f3c1717e656c1dc4eb5f39bfe977ce7864ea46
dameeta <85731431+dameeta@users.noreply.github.com>
2022-03-04 15:07:06 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -3,8 +3,15 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-        <h1>Home page</h1>
+    <div className="container">
+    <div className="card" style="width: 18rem;">
+  <img src="..." className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
     </div>
   )
 }
