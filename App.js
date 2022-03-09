d0df2f24240fdb1b418692b28c375175c5de45e0
dameeta <85731431+dameeta@users.noreply.github.com>
2022-03-09 13:48:51 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,47 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-
+import Layout from './pages/Layout';
+import Todo from './pages/Todo';
 
 function App() {
-  return ( 
-  <>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-    <Router>
-      <Routes>
-        <Route path={'/'} element={<Home />}></Route>
-        <Route path={'/about'} element={<About/>}></Route>
-        <Route path={'/contact'} element={<Contact/>}></Route>
-      </Routes>
-    </Router>
-  </>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path={'/'} element={<Layout />}>
+            <Route path={'/home'} element={<Home />}></Route>
+            <Route path={'/about'} element={<About />}></Route>
+            <Route path={'/contact'} element={<Contact />}></Route>
+            <Route path={'/todo'} element={<Todo />}></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
--- Home.js
+++ Home.js
@@ -1,19 +1,8 @@
-import React from "react";
-
 
 function Home() {
-  return (
-    <div className="container">
-    <div className="card" style={{width: '18rem'}}>
-  <img src="https://via.placeholder.com/150x100" className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-  )
+<div>
+  <h1>Home pages</h1>
+  </div>;
 }
 
 export default Home;
